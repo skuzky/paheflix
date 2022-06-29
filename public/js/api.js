5c5ce77e498b179f486c468b0020dff6f927f426
Bagus Iqbal <bagus.iqbal@ailima.co.id>
2022-06-29 13:31:02 +0700

    First Initiate

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "b5d531906f160a63bbd12f6ee25ecf8c";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
